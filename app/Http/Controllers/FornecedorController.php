af1534770186ec433f64315db64f71064caf5552
Ícaro Campos <icarocampos97@gmail.com>
2021-10-24 20:28:14 -0300

    up sintax blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,18 +8,15 @@ class FornecedorController extends Controller
 {
     public function index() {
         $fornecedores = [
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
-            'Fornecedor 1',
+           0 => [
+               'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00'
+           ],
+           1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+            ]
         ];
 
         return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,20 +2,19 @@
 
 @php
     /*
-    if() {
-
-    } elseif() {
-
-    } else {
-
-    }
+    if(isset($variavel)) {} //retornar true
     */
 @endphp
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status']}}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj']}}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
+    @endisset
+@endisset
